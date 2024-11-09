64bf5a624cf4e596a5236cb89b9aa02f201f36bd
Lallsde <131592926+Lallsde@users.noreply.github.com>
2024-11-09 18:52:56 +0100

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  basePath: '', // Lascia vuoto se il sito non è ospitato in una sottocartella
+  trailingSlash: true, // Per aggiungere uno slash alla fine di ogni URL, utile per alcuni server di hosting
 }
 
 module.exports = nextConfig
